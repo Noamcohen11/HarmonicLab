3ac9ab9e3112b862b07d8972ea809eac33e2b667
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2022-04-21 20:13:52 +0300

    Mass to time now supports multiple csvs

--- mass_to_time.m
+++ mass_to_time.m
@@ -9,67 +9,81 @@
 % Prints goodness of fit and plot
 
 %% Parameters:
+plot_every_mass = 1;
 distance_error_range = 0.0172;
-results_addr = 'D:\labs\simulation\lab_2\csv_files\5 G part 2.csv';
+results_addr = {
+    'D:\labs\simulation\lab_2\csv_files\5 G part 2.csv',
+    'D:\labs\simulation\lab_2\csv_files\10 G part 2.csv',
+    'D:\labs\simulation\lab_2\csv_files\14.6 G part 2.csv',
+    'D:\labs\simulation\lab_2\csv_files\20 G part 1.csv',
+    'D:\labs\simulation\lab_2\csv_files\84 G part 2.csv',
+    };
 
-%% Grab lab results
-results = readtable(results_addr);
-y = results{:,2};
-x = results{:,1};
+%% code:
+% Fit each result:
+for i = 1:length(results_addr)
 
-%% Remove unwanted data
+    %% Grab lab results
+    results = readtable(string(results_addr(i)));
+    y = results{:,2};
+    x = results{:,1};
 
-% Remove data captured before the experiment begins:
-init_wave = find(y == min(y));
-x = x-x(init_wave);
-for i = 1:init_wave-1
-	x(1) = [];
-	y(1) = [];
-end
+    %% Remove unwanted data
 
-%Remove data captured after the experiment ends:
-act_zero = abs(y(length(y)))+distance_error_range;
-for i = 60:length(x)
-    if (abs(y(i))<act_zero) && (abs(y(i-60))<act_zero) 
-        x(i:length(x)) = [];
-        y(i:length(y)) = [];
+    % Remove data captured before the experiment begins:
+    init_wave = find(y == min(y));
+    x = x-x(init_wave);
+    for j = 1:init_wave-1
+        x(1) = [];
+        y(1) = [];
+    end
+    
+    %Remove data captured after the experiment ends:
+    act_zero = abs(y(length(y)))+distance_error_range;
+    for j = 60:length(x)
+        if (abs(y(j))<act_zero) && (abs(y(j-60))<act_zero) 
+            x(j:length(x)) = [];
+            y(j:length(y)) = [];
         break
+        end
+    end
+    
+    final_fit = DampedHarmonic_fit(x,y);
+    % Get goodness of fit:
+    fit_error_range = confint(final_fit)
+    %% Plot:
+    if plot_every_mass 
+        figure
+        plot(x,y, '.')
+        hold on
+        plot(final_fit, '--r')
+        hold off
+        grid
+        box on
+        xlabel('Time(S)')
+        ylabel('Distance(M)')
+        legend('Original Data',  'Fitted Curve')
     end
 end
 
-%% Get fit parametes:
-
-y = detrend(y);                                                                      % Remove Linear Trend
-yu = max(y);
-yl = min(y);
-yr = (yu-yl);                                                                        % Range of  y’
-yz = y-yu+(yr/2);
-zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);                                 % Returns Approximate Zero-Crossing Indices Of Argument Vector
-zt = x(zci(y));
-per = 2*mean(diff(zt));                                                              % Estimate period
-ym = mean(y);                                                                        % Estimate offset
-
-init_fit = @(b,x)  b(1) .* exp(b(2).*x) .* (cos(2*pi*x./b(3) + 2*pi/b(4))) + b(5);   % Objective Function to fit
-fcn = @(b) norm(init_fit(b,x) - y);                                                  % Least-Squares cost function
-[s,nmrs] = fminsearch(fcn, [yr; -10;  per;  -1;  ym]);                               % Minimise Least-Squares
-
-%% Fit:
-fo = fitoptions('Method','NonlinearLeastSquares', 'StartPoint', s');                % Use the parameters gathered as starting points.
-f = fittype('a.*exp(b*x).*(cos(2*pi.*x/c + 2.*pi./d))+e','coefficients', {'a', 'b', 'c', 'd', 'e'}, 'options', fo);
-final_fit = fit(x,y,f);
-
-% Get goodness of fit:
-fit_params = s'
-fit_error_range = confint(final_fit)    
-
-%% Plot:
-figure
-plot(x,y, '.')
-hold on
-plot(final_fit, '--r')
-hold off
-grid
-box on
-xlabel('Time(S)')
-ylabel('Distance(M)')
-legend('Original Data',  'Fitted Curve')
+function f = DampedHarmonic_fit(x, y)
+    %% Get fit parametes:
+    
+    y = detrend(y);                                                                      % Remove Linear Trend
+    yu = max(y);
+    yl = min(y);
+    yr = (yu-yl);                                                                        % Range of  y’
+    zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);                                 % Returns Approximate Zero-Crossing Indices Of Argument Vector
+    zt = x(zci(y));
+    per = 2*mean(diff(zt));                                                              % Estimate period
+    ym = mean(y);                                                                        % Estimate offset
+    
+    init_fit = @(b,x)  b(1) .* exp(b(2).*x) .* (cos(2*pi*x./b(3) + 2*pi/b(4))) + b(5);   % Objective Function to fit
+    fcn = @(b) norm(init_fit(b,x) - y);                                                  % Least-Squares cost function
+    [s,] = fminsearch(fcn, [yr; -10;  per;  -1;  ym]);                               % Minimise Least-Squares
+    fit_params = s';
+    %% Fit:
+    fo = fitoptions('Method','NonlinearLeastSquares', 'StartPoint', fit_params);                % Use the parameters gathered as starting points.
+    fitt = fittype('a.*exp(b*x).*(cos(2*pi.*x/c + 2.*pi./d))+e','coefficients', {'a', 'b', 'c', 'd', 'e'}, 'options', fo);
+    f = fit(x,y,fitt);
+end
