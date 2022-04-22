1cbb1a998908497ccbdbf0226628b717ded0eda3
Noamcohen11 <noam.cohen11@mail.huji.ac.il>
2022-04-22 22:18:30 +0300

    Added graph for peaks

--- mass_to_time.m
+++ mass_to_time.m
@@ -11,20 +11,23 @@
 %% Parameters:
 plot_every_mass = 1;
 distance_error_range = 0.0172;
-results_addr = {
-    'D:\labs\simulation\lab_2\csv_files\5 G part 2.csv';
-    'D:\labs\simulation\lab_2\csv_files\10 G part 2.csv';
-    'D:\labs\simulation\lab_2\csv_files\14.6 G part 2.csv';
-    'D:\labs\simulation\lab_2\csv_files\20 G part 1.csv';
-    'D:\labs\simulation\lab_2\csv_files\84 G part 2.csv';
+lab_results = {
+    'D:\labs\simulation\lab_2\csv_files\5 G part 2.csv'     , 0.005;
+    'D:\labs\simulation\lab_2\csv_files\10 G part 2.csv'    , 0.01;
+    'D:\labs\simulation\lab_2\csv_files\14.6 G part 2.csv'  , 0.0146;
+    'D:\labs\simulation\lab_2\csv_files\20 G part 1.csv'    , 0.02;
+    %'D:\labs\simulation\lab_2\csv_files\84 G part 2.csv'    , 0.0847;
     };
 
 %% code:
 % Fit each result:
-for i = 1:length(results_addr)
 
-    %% Grab lab results
-    results = readtable(string(results_addr(i)));
+CycleTime = zeros(1,size(lab_results,1));
+
+for i = 1:size(lab_results,1)
+
+    %% Grab lab lab_results
+    results = readtable(string(lab_results(i,1)));
     y = results{:,2};
     x = results{:,1};
 
@@ -49,22 +52,51 @@ for i = 1:length(results_addr)
     end
     
     final_fit = DampedHarmonic_fit(x,y);
-    % Get goodness of fit:
-    fit_error_range = confint(final_fit)
-    %% Plot:
-    if plot_every_mass 
+    % Find peaks:
+    [pks,locs] = findpeaks(y,x);
+    amplitude_fit = DampedAmplitudeFit(locs,pks);
+    peak_error = zeros(1,length(locs)) + distance_error_range;
+    time_error = zeros(1,length(locs));
+    CycleTime(i) = 2*pi/mean(diff(locs(2:10)));
+    %fit_values = coeffvalues(final_fit);
+    %CycleTime(i) = fit_values(3);
+    
+    %% Plot per mass:
+    if plot_every_mass
         figure
+        % Plot actual points:
         plot(x,y, '.')
         hold on
-        plot(final_fit, '--r')
+        % Plot peaks
+        plot(final_fit,locs, pks, 'o')
+        text(locs+.02,pks,num2str((1:numel(pks))'))
         hold off
         grid
         box on
         xlabel('Time(S)')
         ylabel('Distance(M)')
-        legend('Original Data',  'Fitted Curve')
+        legend('Original Data', 'peaks', 'Fitted Curve')
+        
+        %Plot peaks per time
+        figure
+        hold on
+        errorbar(locs, pks, peak_error, peak_error, time_error, time_error, 'color','magenta','LineStyle','none', 'LineWidth', 2)
+        plot(amplitude_fit)
+        grid
+        box on
+        xlabel('peaks time(S)')
+        ylabel('peaks(m)')
+        legend('peaks', 'Fitted Curve')
+        hold off
     end
 end
+
+
+%% Plot for all mass
+figure
+mass = cell2mat(lab_results(:,2))';
+plot(cell2mat(lab_results(:,2))',2*pi./CycleTime, '.')
+hold on
 
 function f = DampedHarmonic_fit(x, y)
     %% Get fit parametes:
@@ -80,10 +112,16 @@ function f = DampedHarmonic_fit(x, y)
     
     init_fit = @(b,x)  b(1) .* exp(b(2).*x) .* (cos(2*pi*x./b(3) + 2*pi/b(4))) + b(5);   % Objective Function to fit
     fcn = @(b) norm(init_fit(b,x) - y);                                                  % Least-Squares cost function
-    [s,] = fminsearch(fcn, [yr; -10;  per;  -1;  ym]);                               % Minimise Least-Squares
+    [s,] = fminsearch(fcn, [yr; -10;  per;  -1;  ym]);                                   % Minimise Least-Squares
     fit_params = s';
     %% Fit:
-    fo = fitoptions('Method','NonlinearLeastSquares', 'StartPoint', fit_params);                % Use the parameters gathered as starting points.
+    fo = fitoptions('Method','NonlinearLeastSquares', 'StartPoint', fit_params);         % Use the parameters gathered as starting points.
     fitt = fittype('a.*exp(b*x).*(cos(2*pi.*x/c + 2.*pi./d))+e','coefficients', {'a', 'b', 'c', 'd', 'e'}, 'options', fo);
     f = fit(x,y,fitt);
 end
+
+function f = DampedAmplitudeFit(x, y)
+    fitt = fittype('a*exp(-b*x)+c','coefficients', {'a', 'b', 'c'});
+    f = fit(x,y,fitt);
+end
+
